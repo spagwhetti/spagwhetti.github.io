faaaa4a1f9a8d1187a5a9574ebfd55da2d467672
spagwhetti <spgeti4@gmail.com>
2024-11-09 20:14:27 +0000

    Update chatroom.js

--- scripts/chatroom.js
+++ scripts/chatroom.js
@@ -1,3 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
 var chatbox = document.getElementById("chatbox");
 var messageInput = document.getElementById("message");
 
