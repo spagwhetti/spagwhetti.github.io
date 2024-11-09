d69d2cdb841090487d30a383e66539b9ff744275
spagwhetti <spgeti4@gmail.com>
2024-11-09 19:41:03 +0000

    Update chatroom.js

--- js/chatroom.js
+++ js/chatroom.js
@@ -2,14 +2,14 @@ var chatbox = document.getElementById("chatbox");
 var messageInput = document.getElementById("message");
 
 function sendMessage() {
-var message = messageInput.value;
-chatbox.innerHTML += "<p>" + message + "</p>";
-messageInput.value = "";
+  var message = messageInput.value;
+  chatbox.innerHTML += "<p>You: " + message + "</p>";
+  messageInput.value = "";
 }
 
 messageInput.addEventListener("keydown", function(event) {
-if (event.keyCode === 13) {
-event.preventDefault();
-sendMessage();
-}
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    sendMessage();
+  }
 });
