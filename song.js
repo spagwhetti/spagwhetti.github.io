71e9ed2d0ccc5ece1975291d93a1336beccdaf28
spagwhetti <spgeti4@gmail.com>
2025-03-18 16:41:41 +0000

    Delete song.js

--- song.js
+++
@@ -1,9 +0,0 @@
-let user = 'spagwhetti';
-let url = 'https://lastfm-last-played.biancarosa.com.br/' + user + '/latest-song';
-let song = document.querySelector('#song');
-fetch(url)
-  .then(function (response) {
-    return response.json()
-  }).then(function (json) {
-    song.innerHTML = json['track']['name'] + ' - ' + json['track']['artist']['#text'];
-  });
