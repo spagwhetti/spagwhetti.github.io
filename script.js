ffa41dd6a6e135acd8e651ab0c0c28ad5e930380
spagwhetti <spgeti4@gmail.com>
2025-03-18 16:46:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const DATA_FOR_WEBRING = `https://raw.githubusercontent.com/spag-site/spag-site.github.io/main/webring/webring.json`;
+  const list = document.getElementById("peoplelist");
+  const count = document.getElementById("peoplecount");
+
+  function convertHTML(str) {
+    var regexTable =  {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      '\'': '&apos;'
+    };
+
+    let result = str;
+
+    var regexKeys = Object.keys(regexTable);
+
+    for (var i=0; i<regexKeys.length; i++) {
+
+      let regex = new RegExp(regexKeys[i], 'g');
+      result = result.replace(regex, regexTable[regexKeys[i]]);
+    }
+
+    return result;
+  }
+
+  fetch(DATA_FOR_WEBRING)
+    .then((response) => response.json())
+    .then((sites) => {
+      count.innerHTML = sites.length;
+
+      for (var i = 0; i < sites.length; i++){
+        list.innerHTML += "<div><p><a href=" + sites[i].url + ">" + convertHTML(sites[i].name) + "</a></p></div>";
+      }
+
+
+    });
+});
---
+++ webring.js
@@ -0,0 +1,18 @@
+
+const DATA_FOR_WEBRING = `https://raw.githubusercontent.com/spag-site/spag-site.github.io/main/webring/webring.json`;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const name = decodeURI(urlParams.get('name'));
+
+const left = document.getElementById("prev")
+
+const right = document.getElementById("next")
+
+fetch(DATA_FOR_WEBRING)
+  .then((response) => response.json())
+  .then((sites) => {
+    left.href += "&name=" + name;
+    right.href += "&name=" + name;
+
+  });
